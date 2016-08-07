7434c5435f4f3fb474103367287afdc8fffa8502
Venitsiana <venitsiana.ilieva@gmail.com>
2016-08-07 14:27:33 +0300

    added materialize and created empty folders

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <!--Import Google Icon Font-->
+      <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <!--Import materialize.css-->
+      <link type="text/css" rel="stylesheet" href="stylesheets/materialize.css"  media="screen,projection"/>
+
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+
+    <body>
+      install materialize
+      <!--Import jQuery before materialize.js-->
+      <script type="text/javascript" src="js/jquery-3.1.0.min.js"></script>
+      <script type="text/javascript" src="js/materialize/bin/materialize.js"></script>
+    </body>
+  </html>
