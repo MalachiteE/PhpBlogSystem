d528471b42e86181fc365e6f4f0acaa86cf4706c
Venitsiana <venitsiana.ilieva@gmail.com>
2016-09-12 03:22:26 +0300

    added style for delete btn

--- controllers/ajax/Status.php
+++ controllers/ajax/Status.php
@@ -1,8 +1,7 @@
 <?php
-include 'database/DBConnection.php';
-include 'models/StatusDao.php';
+include '../../database/DBConnection.php';
+include '../../models/StatusDao.php';
 
-//var_dump($_POST['firstname']);die('ddd');
 $connection = DBConnection::getDBConnection();
 echo $states = (new StatusDao)->getAllStatusesByTankId($connection, 1);
 
