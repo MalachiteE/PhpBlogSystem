27567f8f91a833c32079c03d2c4460c749e8ef86
Venitsiana <venitsiana.ilieva@gmail.com>
2016-09-18 16:58:36 +0300

    sepated js in different files

--- footer.php
+++ footer.php
@@ -5,6 +5,8 @@
     <script type="text/javascript" src="http://localhost/PhpBlogSystem/js/jquery-3.1.0.min.js"></script>
     <script type="text/javascript" src="http://localhost/PhpBlogSystem/js/materialize/bin/materialize.js"></script>
     <script type="text/javascript" src="http://localhost/PhpBlogSystem/js/Chart.min.js"></script>
+    <script type="text/javascript" src="http://localhost/PhpBlogSystem/js/delete_tank.js"></script>
+    <script type="text/javascript" src="http://localhost/PhpBlogSystem/js/chart_tank.js"></script>
     <script type="text/javascript" src="http://localhost/PhpBlogSystem/js/global.js"></script>
   </body>
 </html>
---
+++ js/delete_tank.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    
+    $( "[data-delete-tank]" ).click(function(e) {
+        e.preventDefault();
+        
+        var href = $(this).attr('href');
+        var id = $(this).data('id');
+        
+        $.post( 
+            href, 
+            {id: id}
+        )
+        .done(function(data) {
+            if(data){
+                $('#tank'+id).closeModal();
+                $('[data-hide-deleted-tank-'+id+']').addClass('hide');
+            }
+        })
+        .fail(function() {
+            alert( "error" );
+        }
+        );       
+    });
+    
+});
+
+
